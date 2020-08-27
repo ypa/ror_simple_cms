e683eb3066701048a559534686e6278b909f7f0b
Yan P Aung <yan.pye.aung@gmail.com>
2020-08-27 07:25:39 -0700

    Complete module 3 - Controllers, Views and Dynamic content

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -4,5 +4,34 @@ class DemoController < ApplicationController
 
 
   def index
+    # render('index')
+  end
+
+  def hello
+    @array = [1, 2, 3, 4, 5]
+    @id = params['id']
+    @page = params['page']
+
+
+
+    # render('hello')
+    # redirect_to(:controller => 'demo', :action => 'index')
+
+  end
+
+  def about
+    render('about_us')
+  end
+
+  def contact
+    if ['us', 'ca'].include?(params[:country])
+      @phone = '(800) 555-6789'
+    elsif params[:country] == 'uk'
+      @phone = '(020) 789-1234'
+    else
+      @phone = '+1 (987) 654-3210'
+    end
+
+    render('contact_us')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   
   get 'demo/index'
+  get 'demo/hello'
+  get 'demo/about'
+  get 'demo/contact'
+
 
   # default route
   # get ':controller(/:action(/:id))'
