6108ba7ee9eec47fbbf8657aa92bd78a497f1445
Yan P Aung <yan.pye.aung@gmail.com>
2020-09-05 15:32:29 -0700

    Add CRUD for pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,62 @@
 class PagesController < ApplicationController
 
   def index
+    @pages = Page.order('position ASC')
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page = Page.new
   end
 
   def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to(pages_path)
+    else
+      render('new')
+    end
+
   end
 
   def edit
+    @page = Page.find(params[:id])
   end
 
   def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to(page_path(@page))
+    else
+      render('edit')
+    end
+
   end
 
   def delete
+    @page = Page.find(params[:id])
   end
 
   def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    redirect_to(pages_path)
   end
+
+  def page_params
+    params.require(:page).permit(
+      :name,
+      :visible,
+      :position,
+      :permalink,
+      :content,
+      :subject_id
+    )
+  end
+
 end
